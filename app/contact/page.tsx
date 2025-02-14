192dc5cb6689ecfd71a24f8f179cc61380dc7c13
radimstefek <83787774+radimstefek@users.noreply.github.com>
2025-02-14 11:50:49 +0100

    fix page.tsx isAuthenticated

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -26,7 +26,7 @@ export default function ContactForm() {
     <div className="flex flex-col min-h-screen">
       {/* Horní část - Navbar */}
       <header>
-        <Navbar />
+        <Navbar isAuthenticated={false} />
       </header>
 
       {/* Hlavní obsah - Kontaktní formulář */}
